9e5f0e5ca956ad1e24777158116d48224ab72b8a
Hansika Reddy Gurrala <143734719+hansikareddy29@users.noreply.github.com>
2026-06-12 23:13:13 +0530

    Add: max-area-of-island - submission-5

---
+++ Data Structures & Algorithms/max-area-of-island/submission-5.cpp
@@ -0,0 +1,34 @@
+// DFS approach of finding connected components
+class Solution {
+public:
+
+    int numberOfConnected(vector<vector<int>>& grid,vector<vector<int>>& visited,int r,int c,int m,int n){
+        int count=1;
+        visited[r][c]=1;
+        int dr[4]={-1,0,1,0};
+        int dc[4]={0,-1,0,1};
+        for(int i=0;i<4;i++){
+            int nrow=dr[i]+r;
+            int ncol=dc[i]+c;
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !visited[nrow][ncol] && grid[nrow][ncol]==1){
+                count+=numberOfConnected(grid,visited,nrow,ncol,m,n);
+            }
+        }
+        return count;
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>>visited(m,vector<int>(n,0));
+        int maxi=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && !visited[i][j]){
+                    maxi=max(maxi,numberOfConnected(grid,visited,i,j,m,n));
+                }
+            }
+        }
+        return maxi;
+    }
+};
